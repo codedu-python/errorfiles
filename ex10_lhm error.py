310a78e8f4b7e991e8f3b22f921b82a0f8e2f87d
iexpo6 <iexpo6@naver.com>
2016-07-07 14:45:56 +0900

    Add files via upload

---
+++ ex10_lhm error.py
@@ -0,0 +1,15 @@
+tabby cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\a\\ cat."
+
+fat_cat = """"
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby cat)
+print(pesian_cat)
+print(backslsh_cat)
+print(fat_cat)
