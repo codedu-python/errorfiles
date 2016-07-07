6c76d7f8aa85e92cf02a6031d42a6f7ab3c0c4d7
Beomi <jun@beomi.net>
2016-07-07 14:40:00 +0900

    Create ex06_errors_ljb.py

---
+++ ex06_errors_ljb.py
@@ -0,0 +1,20 @@
+x = "There are {} types of "people".".format(10)
+binari = "binary"
+do not = "don't"
+y = "Those who know {binary} and those who {}.".format(binary, do_not)
+
+print(x)
+print(y)
+
+print("I said: {}.".format(x))
+print('I also said: '{}'.'format(y))
+
+hilaious = False
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious))
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print(w , e)
